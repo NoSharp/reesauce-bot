247ad06a136a3cb3957f1ce4c25214c6a62b8479
NoSharp <nosharp1@gmail.com>
2021-11-13 21:20:10 +0000

    feat: add more accessors for new config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,24 @@ const { readFileSync } = require("fs")
 const config = parse(readFileSync(`${__dirname}/../config.toml`))
 
 function getDiscordToken(){
-    return config["discord"]["token"]
+    return config["discord"]["token"];
 }
 
-module.exports.getDiscordToken = getDiscordToken;
+function getDiscordClientId(){
+    return config["discord"]["clientId"];
+}
+
+function getDiscordGuildId(){
+    return config["discord"]["guildId"];
+}
+
+function getDiscordChannelId(){
+    return config["discord"]["channelId"];
+}
+
+module.exports = {
+    getDiscordToken,
+    getDiscordChannelId,
+    getDiscordGuildId,
+    getDiscordClientId
+}
