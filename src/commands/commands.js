5aea43f413e820de940481e988c6822db284aef2
NoSharp <nosharp1@gmail.com>
2021-11-13 21:20:49 +0000

    feat: add command handler.

---
+++ src/commands/commands.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { readdirSync } = require("fs");
+
+const commands = {};
+
+function mountSubCommands(cmd){
+    const rootDir = `${__dirname}/`;
+    const dirContent = readdirSync(rootDir);
+    for(const file of dirContent){
+        if(!file.endsWith(".js") || file === "commands.js") continue;
+        
+        const {registerCommand, interactionResponse} = require(`${rootDir}${file}`);
+        commands[file.replace(".js", "")] = interactionResponse;
+        registerCommand(cmd);
+        
+    }
+}
+
+function getInteractionResponse(cmdName){
+    return commands[cmdName];
+}
+
+function generateCommandData(){
+    const command = new SlashCommandBuilder()
+        .setName("resource")
+        .setDescription("Handles all of the commands for resource management.");
+
+    mountSubCommands(command);
+
+    return command.toJSON();
+}
+
+module.exports = {
+    generateCommandData, 
+    getInteractionResponse
+};
