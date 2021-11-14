985033ba1e7227b04021c9035db6bb8fa13cabb3
NoSharp <nosharp1@gmail.com>
2021-11-14 11:34:49 +0000

    feat: add tests for add command.

---
+++ src/commands/add.test.js
@@ -0,0 +1,24 @@
+const add = require("./add");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = (test, assert) =>{
+
+    const command = new SlashCommandBuilder();
+    add.registerCommand(command);
+    
+    const option = command.options[0];
+
+    test("Add command adds the correct name.", () =>{
+        assert.strictEqual(option.name, "add");
+    });
+    
+    test("Add command has every argument required.", () => {
+        for(const opt of option.options){
+            assert.strictEqual(opt.required, true);
+        }
+    });
+
+    test("Add command has correct amount of arguments.", () => {
+        assert.strictEqual(option.options.length, 3);
+    });
+}
