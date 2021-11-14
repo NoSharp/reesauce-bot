62f2a55cfadc0e215ce7bc7ac1e189f25bfba7e0
NoSharp <nosharp1@gmail.com>
2021-11-14 12:05:47 +0000

    fix: ignore test files when registering commands.

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -7,7 +7,7 @@ function mountSubCommands(cmd){
     const rootDir = `${__dirname}/`;
     const dirContent = readdirSync(rootDir);
     for(const file of dirContent){
-        if(!file.endsWith(".js") || file === "commands.js") continue;
+        if(!file.endsWith(".js")  || file.endsWith(".test.js") || file === "commands.js") continue;
         
         const {registerCommand, interactionResponse} = require(`${rootDir}${file}`);
         commands[file.replace(".js", "")] = interactionResponse;
